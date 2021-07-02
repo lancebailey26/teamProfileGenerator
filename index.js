0c3b00331ec3f4fefbd78bb23cd4b6c8f89de02f
Lance Bailey <lancebailey222@gmail.com>
2021-07-02 14:54:12 -0400

    initial question structure

--- index.js
+++ index.js
@@ -4,15 +4,37 @@ const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
 const fs = require("fs");
+var teamArray = [];
 
-// function inquire(){
-//     inquirer
-//         .prompt([
-//             {
-//             type: 'list',
-//             name:'title',
-//             message:"Choose a title: ",
-//             choices: []
-//             }
-//         ])
-// }
+function inquire(){
+    const response = inquirer
+        .prompt([
+            {
+            type: 'input',
+            name:'name',
+            message:"Name: ",
+            },
+            {
+            type: 'input',
+            name: "id",
+            message:"ID: "
+            },
+            {
+            type: 'input',
+            name:'email',
+            message:"Email: ",
+            },
+            {
+                type: 'list',
+                name: 'role',
+                message: "Role: ",
+                choices: [
+                     "Engineer",
+                     "Intern",
+                     "Manager",
+                ]
+            }
+        ]);
+}
+
+inquire();
